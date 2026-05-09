8f87ba26b7946768cb45e428b8423c6a67c9f4fc
unf0r9et <unforgettableunf2006@gmail.com>
2026-05-09 23:38:47 +0300

    feat: added photo worker and txt worker

--- core/publicHeaders/fileSaver.hpp
+++ core/publicHeaders/fileSaver.hpp
@@ -5,10 +5,21 @@
 #include "raindrop.hpp"
 
 class fileSaver {
+    static int processSave(
+        const requestEntity &request,
+        const std::string &directoryPath,
+        int (*saveLong)(const requestEntity &, const std::string &),
+        int (*saveShort)(const requestEntity &, const std::string &)
+    );
+
 public:
     static int saveVideo(const requestEntity &request, const std::string &directoryPath);
 
     static int saveDocs(const requestEntity &request, const std::string &directoryPath);
+
+    static int savePhoto(const requestEntity &request, const std::string &directoryPath);
+
+    static int saveTxt(const requestEntity &request, const std::string &directoryPath);
 };
 
 
--- core/utils/fileSaver/fileSaver.cpp
+++ core/utils/fileSaver/fileSaver.cpp
@@ -2,21 +2,34 @@
 #include "videoWorker.hpp"
 #include "deleteFile.hpp"
 #include "docsWorker.hpp"
+#include "photoWorker.hpp"
+#include "txtWorker.hpp"
 
-int fileSaver::saveVideo(const requestEntity &request, const std::string &directoryPath) {
-  if (request.body.find(TEMP_FILE_PREFIX) != std::string::npos) {
-        const int result = videoWorker::saveLongVideo(request, directoryPath);
+int fileSaver::processSave(const requestEntity &request,
+                           const std::string &directoryPath,
+                           int (*saveLong)(const requestEntity &, const std::string &),
+                           int (*saveShort)(const requestEntity &, const std::string &)) {
+    if (request.body.find(TEMP_FILE_PREFIX) != std::string::npos) {
+        const int result = saveLong(request, directoryPath);
         deleteFile::delFile(request.body);
         return result;
-  }
-  return videoWorker::saveShortVideo(request, directoryPath);
+    }
+    return saveShort(request, directoryPath);
+}
+
+
+int fileSaver::saveVideo(const requestEntity &request, const std::string &directoryPath) {
+    return processSave(request, directoryPath, videoWorker::saveLongVideo, videoWorker::saveShortVideo);
 }
 
 int fileSaver::saveDocs(const requestEntity &request, const std::string &directoryPath) {
-  if (request.body.find(TEMP_FILE_PREFIX) != std::string::npos) {
-        const int result = docsWorker::saveLongDocs(request, directoryPath);
-        deleteFile::delFile(request.body);
-        return result;
-  }
-  return docsWorker::saveShortDocs(request, directoryPath);
+    return processSave(request, directoryPath, docsWorker::saveLongDocs, docsWorker::saveShortDocs);
+}
+
+int fileSaver::savePhoto(const requestEntity &request, const std::string &directoryPath) {
+    return processSave(request, directoryPath, photoWorker::saveLongPhoto, photoWorker::saveShortPhoto);
+}
+
+int fileSaver::saveTxt(const requestEntity &request, const std::string &directoryPath) {
+    return processSave(request, directoryPath, txtWorker::saveLongTxt, txtWorker::saveShortTxt);
 }
