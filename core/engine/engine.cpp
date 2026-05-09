e40e8360bebd6d989d64b72cacc3585e779177b5
unf0r9et <unforgettableunf2006@gmail.com>
2026-05-09 23:41:09 +0300

    feat: new logic for sending files to the client

--- core/engine/engine.cpp
+++ core/engine/engine.cpp
@@ -10,21 +10,25 @@
 #include <filesystem>
 #include <unordered_set>
 #include "fileSearch.hpp"
+#include <fstream>
 
 logger LOGGER(LoggerPath);
 std::unordered_set<std::string> staticDirectories;
+thread_local std::string httpBodyToSendPath;
 
-extern thread_local mode MD;
+extern thread_local mode MDtoReceive;
+thread_local mode MDtoSend = RequestHTTP;
 extern thread_local int content_size;
 extern thread_local requestEntity rqEntity;
 extern thread_local std::string CURRENT_TEMP_FILE_PATH;
 
 void resetThreadState() {
-    MD = RequestHTTP;
+    MDtoReceive = RequestHTTP;
+    MDtoSend = RequestHTTP;
     content_size = 0;
     rqEntity = requestEntity();
     CURRENT_TEMP_FILE_PATH = "";
-
+    httpBodyToSendPath = "";
 }
 
 void openSocket() {
@@ -68,31 +72,70 @@ void openSocket() {
             std::to_string(ntohs(client_address.sin_port)) + " connected", SERVER_PORT, logger::INFO);
 
         std::thread client_thread([socket_client]() {
-            resetThreadState();
+                resetThreadState();
 
-            char buf[MaximumRequestSize];
-
-            while (true) {
-                int bytes = recv(socket_client, buf, sizeof(buf), 0);
-                if (bytes <= 0) break;
+                char buf[MaximumRequestSize];
+                bool isWork = true;
+                while (isWork) {
+                    switch (MDtoSend) {
+                        case RequestHTTP: {
+                            int bytes = recv(socket_client, buf, sizeof(buf), 0);
+                            if (bytes <= 0) {
+                                isWork = false;
+                                break;
+                            }
 
 #ifdef debug
-            LOGGER.log_server("Received bytes: " + std::to_string(bytes), SERVER_PORT,
-                                 logger::DEBUG);
+                            LOGGER.log_server("Received bytes: " + std::to_string(bytes), SERVER_PORT,
+                                              logger::DEBUG);
 #endif
 
-                std::string request_data(buf, bytes);
+                            std::string request_data(buf, bytes);
 
-                std::string response = httpController::startHttpController(request_data);
-                if (response == LOADING_PROCESS) {
-                    continue;
+                            std::string response = httpController::startHttpController(request_data);
+                            if (response == LOADING_PROCESS) {
+                                continue;
+                            }
+
+                            send(socket_client, response.data(), response.size(), 0);
+                        }
+                        break;
+                        case LoadingHTTP:
+                            if (httpBodyToSendPath.empty()) {
+                                std::string response = HTTP_RESPONSE_502;
+                                send(socket_client, response.data(), response.size(), 0);
+                                MDtoSend = RequestHTTP;
+                            } else {
+                                std::ifstream file(httpBodyToSendPath, std::ios::binary);
+                                if (file.is_open()) {
+                                    const size_t BUFFER_SIZE = 8192;
+                                    std::vector<char> buffer(BUFFER_SIZE);
+                                    while (file.read(buffer.data(), buffer.size()) || file.gcount() > 0) {
+                                        std::streamsize bytesRead = file.gcount();
+
+                                        ssize_t bytesSent = send(socket_client, buffer.data(), bytesRead, 0);
+
+                                        if (bytesSent == -1) {
+                                            isWork = false;
+                                            break;
+                                        }
+                                    }
+                                    file.close();
+                                }
+                            }
+                            MDtoSend = RequestHTTP;
+                            break;
+                        default:
+                            std::string response = HTTP_RESPONSE_500;
+                            send(socket_client, response.data(), response.size(), 0);
+                            isWork = false;
+                            break;
+                    }
                 }
 
-                send(socket_client, response.data(), response.size(), 0);
+                close(socket_client);
             }
-
-            close(socket_client);
-        });
+        );
 
         client_thread.detach();
     }
@@ -101,7 +144,5 @@ void openSocket() {
 int engine::startTheRain() {
     staticDirectories = buildFolderSet(StaticRootPath);
     openSocket();
-    // std::thread t(openSocket);
-    // t.join();
     return 0;
 }
---
+++ core/raindrop.hpp
@@ -0,0 +1,20 @@
+#ifndef CEPTEMBER_CEPTEMBER_H
+#define CEPTEMBER_CEPTEMBER_H
+
+#define Raindrop
+
+#define GET(endpoint)
+#define PUT(endpoint)
+#define DELETE(endpoint)
+#define SET(endpoint)
+
+#define SERVER_PORT 8080
+#define MaximumRequestSize 131072
+#define StaticRootPath "../src/static"
+#define LoggerPath "../core/files/logs/frameworkLogs.txt"
+#define TEMP_FILE_PREFIX "../core/files/requestBodies/unf0r9etpref_"
+#define LOADING_PROCESS "LOADING_PROCESS"
+
+enum mode { RequestHTTP, LoadingHTTP, LoadingShortHTTP };
+
+#endif //CEPTEMBER_CEPTEMBER_H
